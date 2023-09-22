567c91eace8468f3d7f4c397a58b7d8dacc39072
Dimple <dewasedimple@gmail.com>
2023-09-22 10:33:46 +0530

    added css in signup

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-   import "./signup.css";
+import "./signup.css";
 
 
 const Signup = (props) => {
